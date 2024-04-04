ef525bdadbde9b7f48b24372d243564316583233
unknown <amaanwaseem24@gmail.com>
2024-04-04 21:11:15 +0500

    basic routing setup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import express from "express";
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec from "./swagger/config";
 import { myDataSource } from "./app-data-source";
+import UserRouter from "./routes/users";
+import PostRouter from "./routes/posts";
+import CommentsRouter from "./routes/comments";
 import "dotenv/config";
 
 myDataSource
@@ -23,6 +26,9 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use("/users", UserRouter);
+app.use("/comments", CommentsRouter);
+app.use("/posts", PostRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
